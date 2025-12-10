61a58d904e6dcc40e8282e5f4053987d30928e40
wongkc847 <wongkc-wm22@student.tarc.edu.my>
2025-12-10 15:03:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ch5wf1hi7x4h.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Abc*1234"
+customdb = "employee"
+custombucket = "wongkaichen-employee"
+customregion = "us-east-1"
+
